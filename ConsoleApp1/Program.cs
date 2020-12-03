0346baf8ed29a7d7e1ae9ec71557c77ff8c0adfc
Roman Golenok <roman.golenok@gmail.com>
2020-12-03 16:46:19 +0300

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await Task.Delay(3000); //wait until server is up
+
+            try
+            {
+                Console.WriteLine("trying to connect");
+
+                var connection = new HubConnectionBuilder()
+                    .WithUrl("https://localhost:5001/players")
+                    .Build();
+
+                connection.Closed += exception =>
+                {
+                    Console.WriteLine(exception);
+                    return Task.CompletedTask;
+                };
+
+                await connection.StartAsync();
+
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ WebApplication1/PlayerHub.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication1
+{
+    public class PlayerHub : Hub
+    {
+        private readonly ILogger<PlayerHub> _logger;
+
+        public PlayerHub(ILogger<PlayerHub> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            _logger.LogInformation("Connected");
+            return base.OnConnectedAsync();
+        }
+    }
+}
